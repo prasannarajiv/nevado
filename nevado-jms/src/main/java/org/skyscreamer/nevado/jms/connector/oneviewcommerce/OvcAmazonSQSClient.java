67c3091191c169ff47679cc36e00b99a1c6c7e31
Isaac Gibbs <isaac.gibbs@oneviewcommerce.com>
2015-10-13 09:46:26 -0400

    add new implementation of amazonsqsClient

--- nevado-jms/src/main/java/org/skyscreamer/nevado/jms/connector/oneviewcommerce/OvcAmazonSQSClient.java
+++ nevado-jms/src/main/java/org/skyscreamer/nevado/jms/connector/oneviewcommerce/OvcAmazonSQSClient.java
@@ -9,11 +9,40 @@ public class OvcAmazonSQSClient extends AmazonSQSClient {
 
 	public OvcAmazonSQSClient(AWSCredentials awsCredentials,
 	                       ClientConfiguration clientConfiguration) {
-		super(awsCredentials, clientConfiguration);
+		AmazonWebServiceClient awsClient = new AmazonWebServiceClient(clientConfiguration);
+		super.awsCredentialsProvider = new StaticCredentialsProvider(awsCredentials);
 		init();
 	}
 
+	/*public AmazonSQSClient(AWSCredentials awsCredentials, ClientConfiguration clientConfiguration) {
+        	super(clientConfiguration);
+        	this.awsCredentialsProvider = new StaticCredentialsProvider(awsCredentials);
+        	init();
+    	}*/
+
 	private void init() {
+		exceptionUnmarshallers.add(new QueueDeletedRecentlyExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new QueueNameExistsExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new EmptyBatchRequestExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new UnsupportedOperationExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new InvalidMessageContentsExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new InvalidBatchEntryIdExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new OverLimitExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new TooManyEntriesInBatchRequestExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new InvalidIdFormatExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new QueueDoesNotExistExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new InvalidAttributeNameExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new BatchRequestTooLongExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new ReceiptHandleIsInvalidExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new MessageNotInflightExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new PurgeQueueInProgressExceptionUnmarshaller());
+	        exceptionUnmarshallers.add(new BatchEntryIdsNotDistinctExceptionUnmarshaller());
+	        
+	        exceptionUnmarshallers.add(new StandardErrorUnmarshaller());
+	        
+	        // calling this.setEndPoint(...) will also modify the signer accordingly
+	        super.setEndpoint("sqs.us-east-1.amazonaws.com");
+	
 		HandlerChainFactory chainFactory = new HandlerChainFactory();
 		requestHandler2s.addAll(chainFactory.newRequestHandlerChain(
 				"/com/oneviewcommerce/shade/nevado/org/skyscreamer/nevado/jms/resource/request.handlers"));
